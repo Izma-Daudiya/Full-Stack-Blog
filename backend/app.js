ef7c43d660975972bd31849a7321c5ce42ceb211
Izma-Daudiya <daudiyaizma@gmail.com>
2025-07-10 18:40:35 +0530

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+//const ENV = process.env.NODE_ENV || 'production'
+//require('dotenv').config({
+//  path: `.env.${ENV}`
+//});
+require('dotenv').config(); // Loads from .env by default
+
+// External Module
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// Local Module
+const errorController = require("./controllers/errorController");
+const blogRouter = require("./routers/blogRouter");
+const MONGO_DB_URL =
+    `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.njnrt.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', blogRouter);
+app.use(errorController.get404);
+
+const PORT = process.env.PORT || 3000;
+mongoose.connect(MONGO_DB_URL).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running at: http://localhost:${PORT}`);
+  });
+});
